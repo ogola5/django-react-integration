bdc32dd3c72349d278a79f7d650064239ab6ae08
ogola5 <ogolaevance5@gmail.com>
2023-04-21 11:05:34 +0300

    ready to deploy

---
+++ Procfile
@@ -0,0 +1,1 @@
+web:gunicorn signup.wsgi --log-file -
--- signup/settings.py
+++ signup/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7p@tuab--ziolg6o3c^&8%b(g@z7s$-spm(^!ql85397&8j-^*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -128,6 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT= os.path.join(BASE_DIR,'staticfiles/')
 STATICFILES_DIRS =[
     os.path.join(BASE_DIR,'frontend/build/static')
 ]
